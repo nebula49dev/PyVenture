bf0507a3faf4ad5440a077c6659a8ecf697947de
IsaiahSloan <103592510+IsaiahSloan@users.noreply.github.com>
2022-05-25 14:54:27 -0500

    prerelease 3
    added more in sword path, logopro has started on the bow path

--- README.md
+++ README.md
@@ -1,19 +1,28 @@
 # PyVenture
 A text-based adventure game written entirely in Python.
 
-## Notes
-- Requires any Python 3 interpreter to run.
-- Necessary info is inside of "wiki.rtf" file. This contains information
-on the story, and how to play.
-- This project is a low priority for me. Don't expect super frequent updates.
-- Inspiration was taken from The Elder Scrolls: Oblivion and Colossal Cave Adventure.
-- This game can be easily ported to any device, since Python is such a universal
-language. Download a Python 3 interpreter on any machine and this game will run.
+> ## Description
+> You wake up in a cold dark prison, next to a choice of weapons. It's up to you to
+> choose your adventure and to find the ending.
 
-## Changelog v1.0
-- First release! 🎉 🎉
-- Added main story, keeping the code and the launcher seperate for now.
-- I made sure to place lots of comments for easy edits and modding.
+> ## Notes
+> - Requires any Python 3 interpreter to run.
+> - Necessary info is inside of "wiki.rtf" file. This contains information
+> on the story, and how to play.
+> - This project is a low priority for me. Don't expect super frequent updates.
+> - Inspiration was taken from The Elder Scrolls: Oblivion and Colossal Cave Adventure.
+> - This game can be easily ported to any device, since Python is such a universal
+> language. Download a Python 3 interpreter on any machine and this game will run.
 
-#### Sword path by @nebula49dev
-#### Bow path by @logopro5
+> ## Changelog v1.0
+> - First release! 🎉 🎉
+> - Added main story, keeping the code and the launcher seperate for now.
+> - I made sure to place lots of comments for easy edits and modding.
+
+> ## Tools Used
+> - Base Engine - https://www.askpython.com/python/text-based-adventure-game
+> - Text Editor - Microsoft Visual Studio Code
+> - Python IDLE - Testing
+
+### Sword path by @nebula49dev
+### Bow path by @logopro5
--- Resources/sword.py
+++ Resources/sword.py
@@ -2,8 +2,11 @@
 #         SWORD PATH         #
 ##############################
 
+import time
+
+
 def sword1():
-    print(""" PyAdventure v1.0
+    print("""
           Sword path start.
           
           You wander through the prison and find the exit gate. There is a guard in front
@@ -15,42 +18,41 @@ def sword1():
     c1 = input()
     time.sleep(2)
     ans = 'incorrect'
-    while(ans=='incorrect'):
-        if(c1.upper()=="KILL"):
+    while(ans == 'incorrect'):
+        if(c1.upper() == "KILL"):
             print("You sneak up behind the guard and attack. Your sword bounces off his thick armor. The guard turns around and stabs you. You were killed.")
             ans = 'correct'
             sword1()
-        elif(c1.upper()=="TALK"):
-            print("You talk to the guard. He turned out to be very friendly and offered to help you.")
-            ans='correct'
+        elif(c1.upper() == "TALK"):
+            print(
+                "You talk to the guard. He turned out to be very friendly and offered to help you.")
+            ans = 'correct'
             sword2()
         else:
             print("Invalid answer. Try again.")
             c1 = input()
 
-#unfinished below
+
 def sword2():
-    print(""" PyAdventure v1.0
-          Sword path start.
+    print("""
+          Will you accept the guard's help?
           
-          You wander through the prison and find the exit gate. There is a guard in front
-          of the door. He does not see you.
-          
-          You can either try to kill the guard, or talk to him.
-          Type "KILL" or "TALK" to decide.
+          Type "YES" or "NO" to decide.
           """)
     c1 = input()
     time.sleep(2)
     ans = 'incorrect'
-    while(ans=='incorrect'):
-        if(c1.upper()=="KILL"):
-            print("You sneak up behind the guard and attack. Your sword bounces off his thick armor. The guard turns around and stabs you. You were killed.")
+    while(ans == 'incorrect'):
+        if(c1.upper() == "YES"):
+            print(
+                "The guard gave you directions to the nearest city. You continue on your way.")
             ans = 'correct'
-            sword1()
-        elif(c1.upper()=="TALK"):
-            print("You talk to the guard. He turned out to be very friendly and offered to help you.")
-            ans='correct'
-            sword2()
+            sword3()
+        elif(c1.upper() == "NO"):
+            print(
+                "You politely decline the guard's offer, and continue off in your own direction.")
+            ans = 'correct'
+            sword3()
         else:
             print("Invalid answer. Try again.")
             c1 = input()
